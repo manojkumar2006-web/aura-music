b9855278e4b25b3466727e3c5d0bf070a70aa459
Moviesking12 <manojkumarsas2006@gmail.com>
2026-07-02 10:54:13 +0530

    Build: Add Capacitor and initialized Android mobile platform wrapper

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.auramusic.app',
+  appName: 'Aura Music',
+  webDir: 'dist'
+};
+
+export default config;
